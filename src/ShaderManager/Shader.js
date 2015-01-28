f1ed8b624436b4dafcc83d97a107045ca90c4eb8
FelixMcFelix <kyleandrew.simpson@gmail.com>
2015-01-28 11:48:50 +0000

    Adding example, redefining Shader constructor.
     * given example is a work in progress, when finished (in conjunction w/ he ShaderManager) it shall render a red square to the screen.
     * Shader constructor changed to make compilation a little nicer in future probably.

--- src/ShaderManager/Shader.js
+++ src/ShaderManager/Shader.js
@@ -1,12 +1,13 @@
 /**
 * @class ShaderManager.Shader
 * @constructor
+* @param {WebGLRenderingContext} gl - The context the shader will belong to and be compiled by.
 * @param {string} name - The name the shader object will be referred to by.
 * @param {number} type - The class of the shader contained, either ShaderManager.Shader.VS or ShaderManager.Shader.FS.
 * @param {string} source - The source code to compile the shader from.
 * @param {object} [attrs] - The array which contains attribute names and default values, as an array of 2-tuples.
 */
-ShaderManager.Shader = function(name, type, source, attrs){
+ShaderManager.Shader = function(gl, name, type, source, attrs){
 
 };
 
